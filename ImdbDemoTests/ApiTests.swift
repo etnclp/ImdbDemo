5a648c7d7d1f7933c3fc1e0fbd23fd88ebcfc98b
Erdi Tunçalp <erdi.tuncalp@kafein.com.tr>
2019-04-22 20:08:42 +0300

    Apitest added

---
+++ ImdbDemoTests/ApiTests.swift
@@ -0,0 +1,61 @@
+//
+//  ApiTests.swift
+//  ImdbDemoTests
+//
+//  Created by Erdi Tunçalp on 22.04.2019.
+//  Copyright © 2019 Erdi Tunçalp. All rights reserved.
+//
+
+import Quick
+import Nimble
+import Moya
+@testable import ImdbDemo
+
+class ApiTests: QuickSpec {
+    
+    override func spec() {
+        describe("Provider test 1") {
+            var provider: MoyaProvider<ImdbAPI>!
+            
+            beforeEach {
+                provider = MoyaProvider<ImdbAPI>(endpointClosure: self.customEndpointClosure, stubClosure: MoyaProvider.immediatelyStub)
+            }
+            
+            it("emits a Response object") {
+                var called = false
+                
+                _ = provider.rx.request(.search)
+                    .subscribe { event in
+                        switch event {
+                        case .success(let response):
+                            log.debug(response)
+                            called = true
+                        case .error(let error):
+                            fail("errored: \(error)")
+                        }
+                }
+                
+                expect(called).to(beTrue())
+            }
+        }
+    }
+    
+    func customEndpointClosure(_ target: ImdbAPI) -> Endpoint {
+        return Endpoint(url: URL(target: target).absoluteString,
+                        sampleResponseClosure: { .networkResponse(500, target.testSampleData) },
+                        method: target.method,
+                        task: target.task,
+                        httpHeaderFields: target.headers)
+    }
+    
+}
+
+extension ImdbAPI {
+    var testSampleData: Data {
+        switch self {
+        case .search:
+            let url = Bundle(for: ApiTests.self).url(forResource: "SearchResult1", withExtension: "json")!
+            return try! Data(contentsOf: url)
+        }
+    }
+}
