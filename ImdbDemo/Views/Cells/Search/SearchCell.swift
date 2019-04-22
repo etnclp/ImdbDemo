d57c98cc57aba5aa7cfc3ee60254bff5b783a25d
Erdi Tunçalp <erdi.tuncalp@kafein.com.tr>
2019-04-22 23:54:03 +0300

    SearchCell added.

---
+++ ImdbDemo/Views/Cells/Search/SearchCell.swift
@@ -0,0 +1,18 @@
+//
+//  SearchCell.swift
+//  ImdbDemo
+//
+//  Created by Erdi Tunçalp on 22.04.2019.
+//  Copyright © 2019 Erdi Tunçalp. All rights reserved.
+//
+
+import UIKit
+
+class SearchCell: UITableViewCell, SpecialCell {
+    
+    // MARK: - Properties
+    
+    static var Height: CGFloat = 0
+    
+    
+}
