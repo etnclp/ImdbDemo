9654535a2a59ea2fb517925ce2b63ca54bfd23f0
Erdi Tunçalp <erdi.tuncalp@kafein.com.tr>
2019-04-22 23:34:39 +0300

    MoviesViewModel added.

---
+++ ImdbDemo/Scenes/Movies/MoviesViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  MoviesViewModel.swift
+//  ImdbDemo
+//
+//  Created by Erdi Tunçalp on 22.04.2019.
+//  Copyright © 2019 Erdi Tunçalp. All rights reserved.
+//
+
+import Foundation
+
+protocol MoviesViewModel {
+    
+}
---
+++ ImdbDemo/Scenes/Movies/MoviesViewModelImpl.swift
@@ -0,0 +1,13 @@
+//
+//  MoviesViewModelImpl.swift
+//  ImdbDemo
+//
+//  Created by Erdi Tunçalp on 22.04.2019.
+//  Copyright © 2019 Erdi Tunçalp. All rights reserved.
+//
+
+import Foundation
+
+class MoviesViewModelImpl: MoviesViewModel {
+    
+}
