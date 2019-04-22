2e2e600c3d1ecfdd6efa6b8aed51cc6c476ae063
Erdi Tunçalp <erdi.tuncalp@kafein.com.tr>
2019-04-22 18:50:43 +0300

    Initial Commit

---
+++ ImdbDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImdbDemo
+//
+//  Created by Erdi Tunçalp on 22.04.2019.
+//  Copyright © 2019 Erdi Tunçalp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
